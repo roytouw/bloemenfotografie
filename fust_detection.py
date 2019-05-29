ac1b7cca34e09f0cf41c0912e6685901dc2a0857
Roy <roytouw@hotmail.com>
2019-05-29 17:46:01 +0200

    virtual env requirements added.

--- fust_detection.py
+++ fust_detection.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-im = Image.open('black.jpg')
+im = Image.open('fust2.jpg')
 im = im.resize((300, 300))
 pix = im.load()
 
@@ -8,12 +8,14 @@ r = 0
 g = 0
 b = 0
 
-for i in range(0, 300, 3):
-    for j in range(0, 300, 3):
+for i in range(0, 300, 6):
+    for j in range(0, 300, 6):
         r += pix[i, j][0]
         g += pix[i, j][1]
         b += pix[i, j][2]
+        pix[i, j] = (255,0, 0)
 
-brightness = r + g + b / 3
+brightness = (r + g + b) / 3
 
 print(r, g, b, brightness)
+im.save('fust2d.jpg')
