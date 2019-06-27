43340fc3382ff9aae23f54c6b1ae6a9a06df1ad8
Roy <roytouw@hotmail.com>
2019-06-27 11:14:27 +0200

    QR decode method added.

--- qr_code.py
+++ qr_code.py
@@ -1,4 +1,6 @@
 import pyqrcode
+from PIL import Image
+from pyzbar.pyzbar import decode
 
 
 class QR:
@@ -6,8 +8,9 @@ class QR:
     def __init__(self):
         self.path = 'qr_codes/'
         self.scale = 20
-        self.module_color = [0, 0, 0, 255] # Black rgba
-        self.background = [255, 255, 255, 255] # White rgba
+        self.module_color = [0, 0, 0, 255]  # Black rgba
+        self.background = [255, 255, 255, 255]  # White rgba
+        # self.qr = qrtools.QR()
 
     def generateQrCode(self, data, filename=''):
         qr = pyqrcode.create(data)
@@ -16,8 +19,13 @@ class QR:
 
         qr.png(self.path + filename, self.scale, self.module_color, self.background)
 
+    def scanQrCode(self, file):
+        test = decode(Image.open('qr_codes/picture.jpeg'))
+        return test[0].data
+
 
 if __name__ == "__main__":
     qr = QR()
-    qr.generateQrCode('test')
+    qr.generateQrCode(123)
+    data = qr.scanQrCode('123')
 
