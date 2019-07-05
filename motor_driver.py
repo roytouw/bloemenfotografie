0d9affde20841992cdd223d0cc3c44c39f18047f
Roy Touw <roytouw@hotmail.com>
2019-07-05 17:25:30 +0200

    Sped up motor with parabolic function

--- motor_driver.py
+++ motor_driver.py
@@ -5,7 +5,8 @@ import time
 class MotorDriver:
 
     def __init__(self):
-        self.sleep = 0.01
+        self.sleep_initial = 0.0024
+        self.sleep_final = 0.0020
         self.out1 = 12
         self.out2 = 11
         self.out3 = 13
@@ -45,6 +46,7 @@ class MotorDriver:
                 else:
                     i = 0
                     continue
+                time.sleep(self.test(y, rotation))
                 i += 1
 
         elif rotation > -40000 and rotation < 0:
@@ -69,66 +71,85 @@ class MotorDriver:
                 else:
                     i = 7
                     continue
+                time.sleep(self.test(-y, -rotation))
                 i -= 1
+
+    def test(self, y, rotation):
+        coefficient = 0.0000018
+        limit = 400
+        if rotation < limit * 2:
+            return self.sleep_initial
+        elif y <= limit:
+            return self.sleep_initial - y * coefficient
+        elif y >= limit and (rotation - y) >= limit:
+            return self.sleep_initial - limit * coefficient
+        elif (rotation -y) <= limit:
+            return self.sleep_initial - (rotation - y) * coefficient
             
     def pos0(self):
         GPIO.output(self.out1, GPIO.HIGH)
         GPIO.output(self.out2, GPIO.LOW)
         GPIO.output(self.out3, GPIO.LOW)
         GPIO.output(self.out4, GPIO.LOW)
-        time.sleep(self.sleep)
+#        time.sleep(self.sleep)
 
     def pos1(self):
         GPIO.output(self.out1, GPIO.HIGH)
         GPIO.output(self.out2, GPIO.HIGH)
         GPIO.output(self.out3, GPIO.LOW)
         GPIO.output(self.out4, GPIO.LOW)
-        time.sleep(self.sleep)
+#        time.sleep(self.sleep)
 
     def pos2(self):
         GPIO.output(self.out1, GPIO.LOW)
         GPIO.output(self.out2, GPIO.HIGH)
         GPIO.output(self.out3, GPIO.LOW)
         GPIO.output(self.out4, GPIO.LOW)
-        time.sleep(self.sleep)
+#        time.sleep(self.sleep)
 
     def pos3(self):
         GPIO.output(self.out1, GPIO.LOW)
         GPIO.output(self.out2, GPIO.HIGH)
         GPIO.output(self.out3, GPIO.HIGH)
         GPIO.output(self.out4, GPIO.LOW)
-        time.sleep(self.sleep)
+#        time.sleep(self.sleep)
 
     def pos4(self):
         GPIO.output(self.out1, GPIO.LOW)
         GPIO.output(self.out2, GPIO.LOW)
         GPIO.output(self.out3, GPIO.HIGH)
         GPIO.output(self.out4, GPIO.LOW)
-        time.sleep(self.sleep)
+#        time.sleep(self.sleep)
 
     def pos5(self):
         GPIO.output(self.out1, GPIO.LOW)
         GPIO.output(self.out2, GPIO.LOW)
         GPIO.output(self.out3, GPIO.HIGH)
         GPIO.output(self.out4, GPIO.HIGH)
-        time.sleep(self.sleep)
+#        time.sleep(self.sleep)
 
     def pos6(self):
         GPIO.output(self.out1, GPIO.LOW)
         GPIO.output(self.out2, GPIO.LOW)
         GPIO.output(self.out3, GPIO.LOW)
         GPIO.output(self.out4, GPIO.HIGH)
-        time.sleep(self.sleep)
+#        time.sleep(self.sleep)
 
     def pos7(self):
         GPIO.output(self.out1, GPIO.HIGH)
         GPIO.output(self.out2, GPIO.LOW)
         GPIO.output(self.out3, GPIO.LOW)
         GPIO.output(self.out4, GPIO.HIGH)
-        time.sleep(self.sleep)
+#        time.sleep(self.sleep)
 
 
 if __name__ == "__main__":
     driver = MotorDriver()
-    driver.rotate(-8000)
-#    driver.rotate(6000)
+    driver.rotate(-9000)
+#    driver.rotate(9000)
+#    print(driver.test(0, 100))
+#    print(driver.test(90, 100))
+#    print(driver.test(300, 10000))
+#    print(driver.test(400, 10000))
+#    print(driver.test(800, 10000))
+#    print(driver.test(9999, 10000))
