5c97e8454ebce5f911facbd60e0d437b7ab9ee51
Roy Touw <roytouw@hotmail.com>
2019-07-08 13:21:45 +0200

    cleaned main thread.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from qr_code import QR
 from fust_recognizer import FustRecognizer
 from steppermotor_controller import StepperMotorController
 from motor_driver import MotorDriver
+from GUI import GUI
 
 
 configLoader = ConfigLoader()
@@ -15,34 +16,23 @@ qr = QR()
 fust_recognizer = FustRecognizer()
 steppermotor_controller = StepperMotorController()
 motor_driver = MotorDriver()
+gui = GUI()
 
 
 def onDetection(image):
     print('fust')
     qr_data = qr.scanQrCode(image)
     height = steppermotor_controller.getHeightForQRCode(qr_data)
-#    print(qr_data)
-#    print(height)
     motor_driver.rotate(height)
     name = str(datetime.datetime.now()).replace(" ", "")
     command = "fswebcam -d /dev/video0 -r 1600x1200 " + name + ".jpg"
-    print(command)
     os.system(command)
-#    motor_driver.rotate(height)
-#    fust_width = fust_recognizer.getWidthOverRange(image)
-#    qr_height = steppermotor_controller.getHeightForQRCode(qr_data)
-#    detected_height = steppermotor_controller.getHeightForWidth(fust_width)
-#    height = steppermotor_controller.getHeightForQRCode(3)
-#    motor_driver.rotate(height)
-
-#   TODO move stepper motor and take the proper photo.
 
 
 def lostDetection(image):
     print('nothing')
     height = steppermotor_controller.getHeightForQRCode(-1)
     motor_driver.rotate(height)
-#    motor_driver.rotate(steppermotor)
 
 
 fust_detector.setDetectionHook(onDetection).setNonDetectionHook(lostDetection).start_monitoring()
